fede0f004107462169aef2d2408b9b792cb82a69
Riyazur Rahman M <Riyazur Rahman M@DESKTOP-DJU19SF>
2022-10-02 20:00:10 +0530

    First Commit

---
+++ RRproj/src/RRpack/Details.java
@@ -0,0 +1,5 @@
+package RRpack;
+
+public class Details {
+	public String str1 = "I just created this repository to demonstrate that we can use git funstions from eclipse also";
+}
---
+++ RRproj/src/RRpack/RRproj.java
@@ -0,0 +1,9 @@
+package RRpack;
+
+public class RRproj{
+	public static void main(String[] args) {
+		System.out.println("This is my first Repository online.");
+		Details obj = new Details();
+		System.out.println(""+obj.str1);
+	}
+}
